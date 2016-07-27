d720b5702862e288e5a0b84dfa05ce4aa233b70a
yemelyn <pommy.shortcake@gmail.com>
2016-07-27 23:27:36 +0000

    first commit

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,23 @@
-$(document).ready(function() {
+$(function () {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var apiKey = cc31c65a27ed99826b636f0f05a91807
+
+  $.ajax({
+    url: 'https://api.flickr.com/services/rest',
+    method: 'flickr.photos.getRecent',
+    data: {
+      tag: "lighthouse",
+      format: json
+      api_key: apiKey
+    },
+    dataType: 'jsonp',
+    success: function (json) {
+      console.log(json);
+    },
+    error: function (err) {
+      console.log('dealing with it');
+      // TODO Deal with it!
+    }
+  });
+
 });
