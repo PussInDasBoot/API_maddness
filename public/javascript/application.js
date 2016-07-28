519b92e283c77d3536d5b30ad8b2de3916ca80da
yemelyn <pommy.shortcake@gmail.com>
2016-07-28 01:17:14 +0000

    completed using slide

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,18 +1,46 @@
 $(function () {
 
-  var apiKey = cc31c65a27ed99826b636f0f05a91807
+  var apiKey = "cc31c65a27ed99826b636f0f05a91807";
+
+  var Templates = {}
+
+  // function init(){
+  //   var imageTemplateHTML = $('#image-template').html();
+  //   // The template function for producing HTML
+  //   Templates.image = Handlebars.compile(imageTemplateHTML);
+  // }
+
+  // init();
 
   $.ajax({
     url: 'https://api.flickr.com/services/rest',
-    method: 'flickr.photos.getRecent',
+    method: 'get',
     data: {
+      method: "flickr.tags.getClusterPhotos",
       tag: "lighthouse",
-      format: json
-      api_key: apiKey
+      cluster_id: "3",
+      format: "json",
+      api_key: apiKey,
+
     },
     dataType: 'jsonp',
-    success: function (json) {
-      console.log(json);
+    jsonpCallback: 'jsonFlickrApi',
+    success: function (data) {
+      console.log(data);
+      for (var i = 0; i < 10; i++) {
+        var id = data.photos.photo[i].id;
+        var secret = data.photos.photo[i].secret;
+        var serverid = data.photos.photo[i].server;
+        var farmid = data.photos.photo[i].farm;
+        var imageUrl = "http://farm" + farmid + ".static.flickr.com/" + serverid + "/" + id + "_" + secret + ".jpg";
+        $('.images').append('<img src='+imageUrl+' class="imgsize">');
+        
+        }
+        $('.images').slick({
+          autoplay: true,
+          autoplaySpeed: 1500,
+          fade: true
+        })
     },
     error: function (err) {
       console.log('dealing with it');
